16b45b076aea40778eaa04cc34ed9587f3e8a4b7
Yu-Lang Chu <yulang.chu@gmail.com>
2021-04-23 21:58:33 +0200

    add week2 HW

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # advanced-go-training-homework
+
+## week2
+Q: When getting sql.ErrNoRows, should we wrap it and return?
+my answer is no, please see comment below.
+```go
+err := s.db.QueryRow(`select user_id from user where email = $1`, email).Scan(&user.ID)
+if err !=nil{
+    // sql.ErrorNoRows means "user not found" in business logic level.
+    // don't return sql.ErrorNoRows or it wrapped error because it's not a real database error
+    // recommend to use custom error for business logic level
+    if err == sql.ErrNoRows{
+        return User{}, ErrUserNotFound
+    }
+    return User{}, err
+}
+return user, nil
+```
---
+++ week2/user/user.go
@@ -0,0 +1,32 @@
+package user
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+)
+
+type Store struct{
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+var ErrUserNotFound = errors.New("user not found")
+
+func (s Store) QueryUserByEmail(ctx context.Context, email string) (User, error)  {
+	var user User
+	err := s.db.QueryRow(`select user_id from user where email = $1`, email).Scan(&user.ID)
+	if err !=nil{
+		// sql.ErrorNoRows means "user not found" in business logic level.
+		// don't return sql.ErrorNoRows or it wrapped error because it's not a real  database error
+		// should use custom error for business logic level
+		if err == sql.ErrNoRows{
+			return User{}, ErrUserNotFound
+		}
+		return User{}, err
+	}
+	return user, nil
+}
