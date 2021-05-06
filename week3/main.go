7f9a0485635e850c7699464e2e561107fae84b98
Yu-Lang Chu <yulang.chu@gmail.com>
2021-05-07 00:52:25 +0200

    add server 2

--- week3/main.go
+++ week3/main.go
@@ -16,8 +16,11 @@ func main() {
 	srv1 := http.Server{
 		Addr: ":8080",
 	}
+	srv2 := http.Server{
+		Addr: ":8081",
+	}
 	c := make(chan os.Signal, 1)
-	shutdown := make(chan struct{},1)
+	shutdown := make(chan struct{}, 1)
 	signal.Notify(c, os.Interrupt)
 	g, ctx := errgroup.WithContext(context.Background())
 
@@ -27,16 +30,40 @@ func main() {
 	})
 
 	g.Go(func() error {
+		log.Printf("starting srv2 on %s\n", srv2.Addr)
+		return fmt.Errorf("srv2:%w", srv2.ListenAndServe())
+	})
+
+	g.Go(func() error {
 		select {
-			case sig:=<-c:
-				log.Printf("closing srv1: %s\n", sig)
-			case <-shutdown:
+		case sig := <-c:
+			log.Printf("signal received: %s\n", sig)
+			close(shutdown)
+		case <-shutdown:
 		}
+		return nil
+	})
+
+	g.Go(func() error {
+		<-shutdown
+		log.Printf("closing srv1\n")
 		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
 		defer cancel()
-		if err:= srv1.Shutdown(ctx); err!=nil{
+		if err := srv1.Shutdown(ctx); err != nil {
 			srv1.Close()
 			return fmt.Errorf("srv1: cloud not stop server gracefully: %w", err)
+		}
+		return nil
+	})
+
+	g.Go(func() error {
+		<-shutdown
+		log.Printf("closing srv2\n")
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+		if err := srv2.Shutdown(ctx); err != nil {
+			srv2.Close()
+			return fmt.Errorf("srv2: cloud not stop server gracefully: %w", err)
 		}
 		return nil
 	})
