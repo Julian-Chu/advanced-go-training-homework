526687fcc64694440108ca40a3e8c16ab04261e1
Yu-Lang Chu <yulang.chu@gmail.com>
2021-06-24 23:47:30 +0200

    add Encode

--- week9/protocol.go
+++ week9/protocol.go
@@ -1,10 +1,50 @@
 package week9
 
+import "encoding/binary"
+
 type Protocol struct {
-	PackLen   int32
-	HeaderLen int16
-	Ver       int16
-	Op        int32
-	SeqId     int32
+	PackLen   uint32
+	HeaderLen uint16
+	Ver       uint16
+	Op        uint32
+	SeqId     uint32
 	Body      []byte
+}
+
+const (
+	packLenSize   = 4
+	headerLenSize = 2
+	verSize       = 2
+	opSize        = 4
+	SeqIdSize     = 4
+)
+
+func NewProtocol(ver uint16, op uint32, seqId uint32, message string) *Protocol {
+
+	return &Protocol{Ver: ver, Op: op, SeqId: seqId, Body: []byte(message)}
+}
+
+func Encode(p Protocol) []byte {
+	headerSize := packLenSize + headerLenSize + verSize + opSize + SeqIdSize
+	bufSize := headerSize
+	if p.Body != nil {
+		bufSize += len(p.Body)
+	}
+	buf := make([]byte, bufSize)
+	offset := 0
+	binary.BigEndian.PutUint32(buf[offset:], p.PackLen)
+	offset += packLenSize
+	binary.BigEndian.PutUint16(buf[offset:], p.HeaderLen)
+	offset += headerLenSize
+	binary.BigEndian.PutUint16(buf[offset:], p.Ver)
+	offset += verSize
+	binary.BigEndian.PutUint32(buf[offset:], p.Op)
+	offset += opSize
+	binary.BigEndian.PutUint32(buf[offset:], p.SeqId)
+	if p.Body != nil {
+		offset += SeqIdSize
+		copy(buf[offset:], p.Body)
+	}
+
+	return buf
 }
