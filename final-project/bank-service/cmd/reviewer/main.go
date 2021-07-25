f65514df9807ad38600b3f5700ef42c94a646c58
Yu-Lang Chu <yulang.chu@gmail.com>
2021-07-25 14:32:56 +0200

    reviewer create account after consuming

--- final-project/bank-service/cmd/reviewer/main.go
+++ final-project/bank-service/cmd/reviewer/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	accounts "bankservice/api/accountsservice"
+	"context"
 	"fmt"
 	"github.com/Shopify/sarama"
+	"github.com/go-kratos/kratos/v2/middleware/recovery"
+	"github.com/go-kratos/kratos/v2/transport/grpc"
 	"log"
 	"os"
 	"os/signal"
@@ -35,17 +39,42 @@ func main() {
 	signal.Notify(signals, os.Interrupt)
 
 	consumed := 0
+
+	accountsClient := NewAccountsServiceClient()
+
 ConsumerLoop:
 	for {
 		fmt.Println("consuming...")
 		select {
 		case msg := <-partitionConsumer.Messages():
-			log.Printf("Consumed message offset %d, username: %s\n", msg.Offset, string(msg.Value))
+			username := string(msg.Value)
+			log.Printf("Consumed message offset %d, username: %s\n", msg.Offset, username)
 			consumed++
+			accountReply, err := accountsClient.CreateAccount(context.Background(), &accounts.CreateAccountRequest{Username: username})
+			if err != nil {
+				fmt.Println(err.Error())
+			} else {
+				fmt.Printf("account:%s created for username:%s\n", accountReply.AccountID, username)
+			}
+
 		case <-signals:
 			break ConsumerLoop
 		}
 	}
 
 	log.Printf("Consumed: %d\n", consumed)
+}
+
+func NewAccountsServiceClient() accounts.AccountClient {
+	conn, err := grpc.DialInsecure(
+		context.Background(),
+		grpc.WithEndpoint("127.0.0.1:9001"),
+		grpc.WithMiddleware(
+			recovery.Recovery(),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return accounts.NewAccountClient(conn)
 }
